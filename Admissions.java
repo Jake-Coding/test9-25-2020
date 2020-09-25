49287a09356c29aafd3229bde777e7098417ad12
Jake Bridge <34487865+Jake-Coding@users.noreply.github.com>
2020-09-25 10:58:59 -0600

    Admissions complete maybe oh god the amount of if statements makes me want to shove a spike into my skull

--- Admissions.java
+++ Admissions.java
@@ -33,10 +33,40 @@ public class Admissions {
         residenceV = residence.equals("Colorado") ? "Elite" : residence.equals("Florida") ? "Nope" : "Acceptable";
         System.out.print("LAST NAME: ");
         lastName = scanner.next();
+        String lastNameV = lastName.equals("Padjen") ? "Elite" : "Acceptable";
 
-        String[] info = {satV, rankV, awardsV, residenceV, lastName};
-        
+        String[] values = {satV, rankV, awardsV, residenceV, lastNameV};
+        results(values);
     }
+
+    public static void results(String[] info) {
+        for (int i = 0; i < info.length; i++) {
+            if (info[i].equals("Nope")) {
+                System.out.println("No you failed heck you. Unnaceptable scores.");
+                return;
+            }
+        }
+        if (info[4].equals("Elite")) {
+            System.out.println("You're a padjen. Welcome!");
+            return;
+        }
+        int solidCount = 2;
+        for (int i = 0; i < info.length-1; i++) {
+            if (info[i].equals("Elite")) {
+                System.out.println("You made it in because you are elite!");
+                return;
+            } else if (info[i].equals("Solid")) {
+                solidCount--;
+                if (solidCount <= 0) {
+                    System.out.println("You are solid in 2 skills. You made it!");
+                    return;
+                }
+            }
+        }
+        System.out.println("You weren't too bad, but you didn't hit all of the requirements. You utter failure.");
+        return;
+    }
+
 
 
 
